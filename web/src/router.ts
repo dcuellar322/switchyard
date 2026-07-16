0def999e391dc45d5056e167d1292df07b4b924b
David Cuellar <david.a.cuellar@gmail.com>
2026-07-16 17:59:17 -0500

    perf(web): lazy-load route views

--- web/src/router.ts
+++ web/src/router.ts
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import DashboardView from "./domains/dashboard/views/DashboardView.vue";
-import PortRegistryView from "./domains/ports/views/PortRegistryView.vue";
-import ProjectDetailView from "./domains/projects/views/ProjectDetailView.vue";
-import ProjectOnboardingView from "./domains/projects/views/ProjectOnboardingView.vue";
-import LogExplorerView from "./domains/logs/views/LogExplorerView.vue";
-import ResourcesView from "./domains/resources/views/ResourcesView.vue";
-import WorkspacesView from "./domains/workspaces/views/WorkspacesView.vue";
-import PluginsView from "./domains/plugins/views/PluginsView.vue";
-import DiagnosticsView from "./domains/diagnostics/views/DiagnosticsView.vue";
-import SettingsView from "./domains/system/views/SettingsView.vue";
-import FleetView from "./domains/fleet/views/FleetView.vue";
-import TeamView from "./domains/team/views/TeamView.vue";
+const DashboardView = () => import("./domains/dashboard/views/DashboardView.vue");
+const PortRegistryView = () => import("./domains/ports/views/PortRegistryView.vue");
+const ProjectDetailView = () => import("./domains/projects/views/ProjectDetailView.vue");
+const ProjectOnboardingView = () => import("./domains/projects/views/ProjectOnboardingView.vue");
+const LogExplorerView = () => import("./domains/logs/views/LogExplorerView.vue");
+const ResourcesView = () => import("./domains/resources/views/ResourcesView.vue");
+const WorkspacesView = () => import("./domains/workspaces/views/WorkspacesView.vue");
+const PluginsView = () => import("./domains/plugins/views/PluginsView.vue");
+const DiagnosticsView = () => import("./domains/diagnostics/views/DiagnosticsView.vue");
+const SettingsView = () => import("./domains/system/views/SettingsView.vue");
+const FleetView = () => import("./domains/fleet/views/FleetView.vue");
+const TeamView = () => import("./domains/team/views/TeamView.vue");
 
 export const router = createRouter({
   history: createWebHistory(),
